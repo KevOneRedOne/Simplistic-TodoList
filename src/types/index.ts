4899928810dda8a7f843682c508ed67ac58ce53c
KevOneRedOne <kevinalves@hotmail.fr>
2026-01-03 22:50:20 +0100

    feat(types): add Priority type to Task interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,9 @@
+export type Priority = 'low' | 'medium' | 'high';
+
 export type Task = {
   id: number;
   name: string;
   dateCompleted: Date | string;
   completed: boolean;
+  priority: Priority;
 };
